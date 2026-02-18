135675ee4600ec970bcc4f55fd087453fdae51a9
esavary <etienne.savary.ext@culture.gouv.fr>
2026-02-18 22:02:36 +0100

    création Enum Groupe

---
+++ src/main/java/com/monopoly/plateau/modele/constantes/Groupe.java
@@ -0,0 +1,14 @@
+package com.monopoly.plateau.modele.constantes;
+
+public enum Groupe {
+    MARRON,
+    GARE,
+    BLEU_CIEL,
+    VIOLET,
+    COMPAGNIE,
+    ORANGE,
+    ROUGE,
+    JAUNE,
+    VERT,
+    BLEU
+}
