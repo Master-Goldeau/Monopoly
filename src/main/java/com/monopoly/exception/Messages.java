4f86b169b02a9114cf3f82df62199f99f8d3c5a1
esavary <etienne.savary.ext@culture.gouv.fr>
2026-02-28 19:54:09 +0100

    Création Message erreur

---
+++ src/main/java/com/monopoly/exception/Messages.java
@@ -0,0 +1,10 @@
+package com.monopoly.exception;
+
+public final class Messages {
+
+    public static final String ERREUR_TYPE_VALEUR_EFFET_CARTE = "La valeur d'un effet d'une carte piochable doit être de type Case, Integer ou null.";
+    public static final String ERREUR_TYPE_INTEGER = "La valeur n'est pas de type Integer.class";
+    public static final String ERREUR_TYPE_CASE = "La valeur n'est pas de type Case.class";
+
+    private Messages() {}
+}
