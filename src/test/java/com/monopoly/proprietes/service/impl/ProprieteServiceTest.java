71837890ef879ba5e4c7fa91ec7f310aad01cde6
esavary <etienne.savary.ext@culture.gouv.fr>
2026-03-10 19:31:34 +0100

    Suppression methode de jeu de données non utilisée

--- src/test/java/com/monopoly/proprietes/service/impl/ProprieteServiceTest.java
+++ src/test/java/com/monopoly/proprietes/service/impl/ProprieteServiceTest.java
@@ -1,15 +1,11 @@
 package com.monopoly.proprietes.service.impl;
 
-import com.monopoly.proprietes.model.Groupe;
-import com.monopoly.proprietes.model.Propriete;
 import com.monopoly.proprietes.model.cartes.CartePropriete;
 import com.monopoly.proprietes.model.cartes.CartesProprieteConstructibles;
 import com.monopoly.proprietes.model.cartes.CartesProprietesNonConstructibles;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.List;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -22,13 +18,6 @@ class ProprieteServiceTest {
         return Stream.concat(
             Stream.of(CartesProprieteConstructibles.values()),
             Stream.of(CartesProprietesNonConstructibles.values())
-        );
-    }
-
-    public static Stream<Arguments> jeuxDeDonneesPourTestGroupeComplet() {
-        return Stream.of(
-            Arguments.of(Groupe.ORANGE, List.of(new Propriete(), new Propriete(), new Propriete()), true),
-            Arguments.of(Groupe.BLEU, List.of(new Propriete()), false)
         );
     }
 
